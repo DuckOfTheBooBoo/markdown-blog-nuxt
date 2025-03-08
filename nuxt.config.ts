96966b03d963a681aa4b85b4497119001d62b0f9
Arajdian Altaf <arajdianaltaf123@gmail.com>
2025-03-08 14:21:13 +0700

    add shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,18 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode',
+    'nuxt-lucide-icons'
+  ],
+  colorMode: {
+    preference: 'dark', // default value of $colorMode.preference
+    fallback: 'dark', // fallback value if not system preference found
+    classSuffix: '', // optional
+    forced: true, // remove the ability to change the mode
+  },
   shadcn: {
     /**
      * Prefix for all the imported component
