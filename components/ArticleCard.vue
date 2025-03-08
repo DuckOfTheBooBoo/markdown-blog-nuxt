e64e28892a013e473c6402509441e9ac4c69028c
Arajdian Altaf <arajdianaltaf123@gmail.com>
2025-03-08 14:46:55 +0700

    add ArticleCard component

---
+++ components/ArticleCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <Card class="my-5">
+    <CardHeader>
+      <CardTitle class="text-2xl font-bold">{{ title }}</CardTitle>
+      <Separator />
+    </CardHeader>
+    <CardContent class="text-ellipsis text-secondary-foreground/70">{{ summary }}</CardContent>
+    <CardFooter>
+        <Button class="w-full">Read more</Button>
+    </CardFooter>
+  </Card>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+    title: string
+    summary: string
+}>()
+</script>
